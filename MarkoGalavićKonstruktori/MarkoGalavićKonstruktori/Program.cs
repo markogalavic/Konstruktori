fb8ca21121a3db049cf16b9510124dc528c32a0e
markogalavic <55134792+markogalavic@users.noreply.github.com>
2020-10-06 11:06:30 +0200

    Add files via upload

---
+++ MarkoGalavićKonstruktori/MarkoGalavićKonstruktori/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarkoGalavićKonstruktori
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrvaKlasa x = new PrvaKlasa("Perica","Peric");
+            Console.WriteLine(x.ToString());
+
+            PrvaKlasa y = new PrvaKlasa("Ivan","Ivankovic");
+            Console.WriteLine(x.ToString());
+
+            PrvaKlasa z = new PrvaKlasa("Ivana","Janic");
+            Console.WriteLine(x.ToString());
+
+            Console.WriteLine(z.ToString());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MarkoGalavićKonstruktori/MarkoGalavićKonstruktori/PrvaKlasa.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarkoGalavićKonstruktori
+{
+    class PrvaKlasa
+    {
+        private static int counter = 0;
+        private  string ime;
+        private  string prezime;
+        public PrvaKlasa()
+        {
+            counter++;
+        }
+        public PrvaKlasa(string ime,string prezime)
+        {
+            counter++;
+            this.ime = ime;
+            this.prezime = prezime;
+        }
+        public int Counter { get => counter; }
+        public string Ime { get => ime; set => ime = value; }
+        public string Prezime { get => prezime; set => prezime = value; }
+
+        public override string ToString()
+        {
+            string ispis = "Redno brojac : " + counter+", " +ime+" " +prezime;
+            return ispis;
+        }
+
+    }
+}
